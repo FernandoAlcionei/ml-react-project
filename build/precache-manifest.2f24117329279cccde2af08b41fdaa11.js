ee783015c4b522f537dd9b81399e6b6e363a0d17
Fernando Alcionei <fernando.souza@softplan.com.br>
2020-06-22 13:35:42 -0300

    Adicionando docker-compose

---
+++ build/precache-manifest.2f24117329279cccde2af08b41fdaa11.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf9d7b1b072703146c60a3e59a2139b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11b75d6582207ea3fe7",
+    "url": "/static/css/3.12a367f0.chunk.css"
+  },
+  {
+    "revision": "6c526c48183018cd9a38",
+    "url": "/static/css/4.72937c92.chunk.css"
+  },
+  {
+    "revision": "3d1fb27ae61b79f08dce",
+    "url": "/static/css/5.fcd50aeb.chunk.css"
+  },
+  {
+    "revision": "270488a5475433fc0093",
+    "url": "/static/css/main.0e0acb33.chunk.css"
+  },
+  {
+    "revision": "bbfba4b8e202562d61be",
+    "url": "/static/js/2.154643f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.154643f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11b75d6582207ea3fe7",
+    "url": "/static/js/3.12d406b2.chunk.js"
+  },
+  {
+    "revision": "6c526c48183018cd9a38",
+    "url": "/static/js/4.a0271d06.chunk.js"
+  },
+  {
+    "revision": "3d1fb27ae61b79f08dce",
+    "url": "/static/js/5.8cf398eb.chunk.js"
+  },
+  {
+    "revision": "270488a5475433fc0093",
+    "url": "/static/js/main.44b22389.chunk.js"
+  },
+  {
+    "revision": "218e8abdcc56513cc614",
+    "url": "/static/js/runtime-main.f2f6e948.js"
+  },
+  {
+    "revision": "67ede0f529ef3035123876f04a682657",
+    "url": "/static/media/chevron-right.67ede0f5.svg"
+  },
+  {
+    "revision": "506c846118ee3fcaf87900ba0b90f00f",
+    "url": "/static/media/close.506c8461.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/static/media/loading.be00fc4a.svg"
+  }
+]);
